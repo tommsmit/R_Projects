bf9a20e38fb095a72fd8d72e764c2bb115603d57
Thomas Smith <thomas.smith@my.utsa.edu>
2021-04-28 09:37:53 -0500

    Updated

--- Drop Out Loop-Grade.R
+++ Drop Out Loop-Grade.R
@@ -14,6 +14,8 @@ require(XML)
 library(ggplot2)
 
 school_year<-c("1998-99","1999-00","2000-01","2001-02","2002-03","2003-04","2004-05","2005-06","2006-07","2007-08","2008-09","2009-10","2010-11","2011-12","2012-13","2013-14","2014-15","2015-16","2016-17","2017-18","2018-19")
+school_year2<-c("1998-99","1999-00","2000-01","2001-02","2003-04","2004-05","2005-06","2006-07","2007-08","2008-09","2009-10","2010-11","2011-12","2012-13","2013-14","2014-15","2015-16","2016-17","2017-18","2018-19")
+school_year3<-c("98-99","99-00","00-01","01-02","02-03","03-04","04-05","05-06","06-07","07-08","08-09","09-10","10-11","11-12","12-13","13-14","14-15","15-16","16-17","17-18","18-19")
 
 t1<-c(84,95,107,117,119,127,133,61,62,64,69,76,78,77,78,81,81,81,86,86,86)
 
@@ -402,6 +404,8 @@ for (j in 1:length(school_year)){
 
 total1<-filter(total_drop_grade, Grade !="Total") 
 
+
+
 x1<-filter(total1, School_Year=="1998-99")
 x2<-filter(total1, School_Year=="1999-00")
 x3<-filter(total1, School_Year=="2000-01")
@@ -428,10 +432,14 @@ total2<-cbind(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19
 total2<-total2[,c(-10,-19,-28,-37,-46,-55,-64,-73,-82,-91,-100,-109,-118,-127,-136,-145,-154,-163,-172,-181)]
 total3<-filter(total_drop_grade,Grade=="Total")
 
+#Annual Total Dropouts from 1998-2019 (Excluding 2002-03)
+barplot(total3$Female~school_year2,main="Female Annual Dropout By Year",xlab="School Year",ylab="Dropout Rate",col="Salmon")
+barplot(total3$Male~school_year2,main="Male Annual Dropout By Year",xlab="School Year",ylab="Dropout Rate",col="Blue")
+barplot(total3$Female_Percentage~school_year2,main="Female Annual Dropout % By Year",xlab="School Year",ylab="Dropout Percentage",col="Gold")
+barplot(total3$Male_Percentage~school_year2,main="Male Annual Dropout % By Year",xlab="School Year",ylab="Dropout Percentage",col="Red")
+barplot(total3$State~school_year2,main="State Annual Dropout By Year",xlab="School Year",ylab="Dropout Rate",col="Green")
+barplot(total3$State_Percentage~school_year2,main="State Annual Dropout % By Year",xlab="School Year",ylab="Dropout Percentage",col="Purple")
 
-barplot(total3$Female~school_year2)
-
-school_year2<-c("1998-99","1999-00","2000-01","2001-02","2003-04","2004-05","2005-06","2006-07","2007-08","2008-09","2009-10","2010-11","2011-12","2012-13","2013-14","2014-15","2015-16","2016-17","2017-18","2018-19")
 
 grd7<-filter(total_drop_grade,Grade=="Grade-7")
 grd7MoreFemale<-filter(grd7,grd7$Female>grd7$Male)
@@ -451,15 +459,23 @@ grd11MoreFemale<-filter(grd11,grd11$Female>grd11$Male)
 grd12<-filter(total_drop_grade,Grade=="Grade-12")
 grd12MoreFemale<-filter(grd12,grd12$Female>grd12$Male)
 
-total5<-cbind(grd7,grd8,grd9,grd10,grd11,grd12)
+total4<-cbind(grd7,grd8,grd9,grd10,grd11,grd12)
 
-boxplot(total5[,c(3,12,21,30,39,48)])
-boxplot(total5[,c(5,14,23,32,41,50)])
-boxplot(total5[,c(3,5,12,14,21,23,30,32,39,41,48,50)])
+mycolor<-rep(c("red","blue"),6)
+
+#Boxplot Comparing Male/Female Dropout Rate by Grade-Level
+boxplot(total4[,c(3,5,12,14,21,23,30,32,39,41,48,50)],main="Female Vs Male Dropout by Grade Level",xlab="Grade 7-12",ylab="Dropouts",col=mycolor)
+legend("topleft",legend = c("Female", "Male"),col = mycolor,pch = 19,bty = "n",pt.cex = 1,cex = 0.7,text.col = "black",horiz = F ,inset = c(0.05, 0.05))
+
+boxplot(total4[,c(4,6,13,15,22,24,31,33,40,42,49,51)],main="Female Vs Male Dropout by Grade Level",xlab="Grade 7-12",ylab="Dropouts",col=mycolor)
+legend("topleft",legend = c("Female", "Male"),col = mycolor,pch = 19,bty = "n",pt.cex = 1,cex = 0.7,text.col = "black",horiz = F ,inset = c(0.05, 0.05))
+
+#boxplot(total4[,c(3,12,21,30,39,48)],main="Female Dropout by Grade Level",xlab="Grade 7-12",ylab="Dropouts",col="Yellow")
+#boxplot(total4[,c(5,14,23,32,41,50)],main="Male Dropout by Grade Level",xlab="Grade 7-12",ylab="Dropouts",col="Pink")
 
 
 
-
+#Don't Use
 barplot(grd7$Female~school_year)
 barplot(grd7$Male~school_year)
 barplot(grd8$Female~school_year)
@@ -473,25 +489,23 @@ barplot(grd11$Male~school_year)
 barplot(grd12$Female~school_year)
 barplot(grd12$Male~school_year)
 
-barplot(grd7$Female,grd7$Male)
 
-ggplot(grd7, aes(x=School_Year, y =Male,col=Grade)) + geom_jitter()
-
-
-barplot(total1$Female, xlab = "Grade 7-12 From 1998-99 to 2018-19")
-
-boxplot(grd7$Female)
+mycolor1<-rep(c("Orange","Green"),21)
+#Boxplot of Female Vs Male Drop Outs from  grade 7-12 by Year: 1998-2019
+boxplot(total2[,c(3,5,11,13,19,21,27,29,35,37,43,45,51,53,59,61,67,69,75,77,83,85,91,93,99,101,107,109,115,117,123,125,131,133,139,141,147,149,155,157,163,165)],main="Female Vs. Male Dropouts Grades 7-12 by Year",xlab="School Year: 1998-2019",ylab="Dropout Rate",col=mycolor1)
+legend("topleft",legend = c("Female", "Male"),col = mycolor1,pch = 19,bty = "n",pt.cex = 1,cex = 0.7,text.col = "black",horiz = F ,inset = c(0.05, 0.05))
 
 
-#Boxplot of Female Drop Outs from  grade 7-12 from 1998-2019
-boxplot(total2[,c(3,11,19,27,35,43,51,59,67,75,83,91,99,107,115,123,131,139,147,155,163)])
 
 ggplot(total1, aes(x=School_Year, y =Male,col=Grade)) + geom_jitter()
 ggplot(total1, aes(x=School_Year, y =Male_Percentage,col=Grade)) + geom_jitter()
 ggplot(total1, aes(x=School_Year, y =Female,col=Grade)) + geom_jitter()
 ggplot(total1, aes(x=School_Year, y =Female_Percentage,col=Grade)) + geom_jitter()
 
-ggplot(total1, aes(x=Grade, y =Male,col=School_Year)) + geom_col()
+ggplot(total1, aes(x=Grade, y =Male,col=School_Year)) + geom_jitter()
+
+
+ggplot(total1, aes(x=Grade, y =Male,label=School_Year)) + geom_label()
 
 
 
@@ -499,9 +513,20 @@ ggplot(total1, aes(x=Grade, y =Male,col=School_Year)) + geom_col()
 ggplot(total3, aes(x=School_Year, y =State,alpha=State_Percentage)) + geom_col()
 ggplot(total3, aes(x=School_Year, y =State,fill=State_Percentage)) + geom_col()
 
-plot(total4[1,]~school_year)
 
-str(total3)
+
+
+
+write.csv(total1,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\total1.csv", row.names = FALSE)
+write.csv(total2,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\total2.csv", row.names = FALSE)
+write.csv(total3,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\total3.csv", row.names = FALSE)
+write.csv(total4,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\total4.csv", row.names = FALSE)
+write.csv(grd7,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd7.csv", row.names = FALSE)
+write.csv(grd8,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd8.csv", row.names = FALSE)
+write.csv(grd9,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd9.csv", row.names = FALSE)
+write.csv(grd10,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd10.csv", row.names = FALSE)
+write.csv(grd11,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd11.csv", row.names = FALSE)
+write.csv(grd12,"C:\\Users\\12108\\OneDrive\\Desktop\\UTSA\\Spring 2021\\R Project\\R_Projects\\Project 2\\grd12.csv", row.names = FALSE)
 
 
 
