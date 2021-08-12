2187c25b0f35bb483790c7b2d253b4662b720c97
Thomas Smith <thomas.smith@my.utsa.edu>
2021-08-12 15:56:00 -0500

    Updated

--- T3i Interview Project/OFT_ANS_Results.R
+++ T3i Interview Project/OFT_ANS_Results.R
@@ -5,41 +5,46 @@ library(ggplot2)
 library(plotly)
 library(vcd)
 library(treemapify)
+library(plotly)
+
 
 dat2<-read.csv("C:/SAS files/oft_and_ans_result_deID.csv")
 
-dat2_complete<-dat2 %>%
+dat2_complete<- dat2 %>%
   filter(test_complete_status == "Complete")
+
+dat2_complete_pass<-dat2 %>%
+  filter(test_complete_status == "Complete" & overall_PF == "Pass")
 
 dat2_finish<-dat2 %>%
   filter(ans_finish != "")
 
 
-dat2_19005<-dat2 %>%
+dat2_19005<-dat2_complete %>%
   filter(prep_class == "19-005")
 
-dat2_20001<-dat2 %>%
+dat2_20001<-dat2_complete %>%
   filter(prep_class == "20-001")
 
-dat2_20002<-dat2 %>%
+dat2_20002<-dat2_complete %>%
   filter(prep_class == "20-002")
 
-dat2_20003<-dat2 %>%
+dat2_20003<-dat2_complete %>%
   filter(prep_class == "20-003")
 
-dat2_20004<-dat2 %>%
+dat2_20004<-dat2_complete %>%
   filter(prep_class == "20-004")
 
-dat2_20005<-dat2 %>%
+dat2_20005<-dat2_complete %>%
   filter(prep_class == "20-005")
 
-dat2_21001<-dat2 %>%
+dat2_21001<-dat2_complete %>%
   filter(prep_class == "21-001")
 
-dat2_21002<-dat2 %>%
+dat2_21002<-dat2_complete %>%
   filter(prep_class == "21-002")
 
-dat2_21003<-dat2 %>%
+dat2_21003<-dat2_complete %>%
   filter(prep_class == "21-003")
 
 dat2_initial<-dat2_complete %>%
@@ -60,13 +65,15 @@ dat2_wide<-cbind(dat2_initial,dat2_mid,dat2_final)
 
 dat2_wide<-dat2_wide[,c(-38,-39,-40,-75,-76,-77)]
 
+dat2_tree<-select(dat2_complete_pass,broadjump_inches:X3mileruck_seconds, total_oft_score)
+
+dat2_tree2<-select(dat2_complete,broadjump_PF:overall_PF)
 
 ############################## Exploratory Data Analysis ###########################
 
 ggplot(dat2_complete,aes(x=total_oft_score, fill = prep_class)) + geom_bar()
 ggplot(dat2_complete,aes(x=total_oft_score, fill = iteration)) + geom_bar()
-ggplot(dat2_complete,aes(x=farmerscarry_score, fill = iteration)) + geom_bar()
-ggplot(dat2_complete,aes(x = prep_class, y=total_oft_score, fill = iteration)) + geom_col()
+ggplot(dat2_complete,aes(x=prep_class, fill = iteration)) + geom_bar()
 ggplot(dat2_complete,aes(x = prep_class, y=total_oft_score, fill = iteration)) + geom_col()
 ggplot(dat2_complete,aes(x = prep_class, y=farmerscarry_score, fill = iteration)) + geom_col()
 ggplot(dat2_complete,aes(x = farmerscarry_score, y= total_oft_score, fill = iteration)) + geom_col()
@@ -76,18 +83,50 @@ ggplot(dat2_complete,aes(x = farmerscarry_score, y= total_oft_score, fill = iter
 f<-table(dat2_finish[,c(37,5)])
 g<-table(dat2_complete[,c(1,37)])
 h<-table(dat2_complete[,c(6,37)])
+i_19005<-table(dat2_19005[,c(6,37)])
+j_20001<-table(dat2_20001[,c(6,37)])
+k_20002<-table(dat2_20002[,c(6,37)])
+l_20003<-table(dat2_20003[,c(6,37)])
+m_20004<-table(dat2_20004[,c(6,37)])
+n_20005<-table(dat2_20005[,c(6,37)])
+o_21001<-table(dat2_21001[,c(6,37)])
+p_21002<-table(dat2_21002[,c(6,37)])
+q_21003<-table(dat2_21003[,c(6,37)])
 
+l_20003
 mosaicplot(f,shade = TRUE)
 mosaicplot(g,shade = TRUE)
 mosaicplot(h,shade = TRUE)
+mosaicplot(i_19005,shade = TRUE)
+mosaicplot(j_20001,shade = TRUE)
+mosaicplot(k_20002,shade = TRUE)
+mosaicplot(l_20003,shade = TRUE)
+mosaicplot(m_20004,shade = TRUE)
+mosaicplot(n_20005,shade = TRUE)
+mosaicplot(o_21001,shade = TRUE)
+mosaicplot(p_21002,shade = TRUE)
+mosaicplot(q_21003,shade = TRUE)
+
+
+
 
 spineplot(f)
 spineplot(g)
 spineplot(h)
+spineplot(i_19005)
+spineplot(j_20001)
+spineplot(k_20002)
+spineplot(l_20003)
+spineplot(m_20004)
+spineplot(m_20004)
+spineplot(o_21001)
+spineplot(p_21002)
+spineplot(q_21003)
 
 
 
 
+plot(ts(dat2_19005$broadjump_inches))
 
 
 
